f569d17c05b156a2e16ab9ab307f081bf71769d7
sofialapi <sofia.lapi99@gmail.com>
2026-09-10 18:07:29 +0200

    modifica train per secondo set di addestramenti

--- src/training/train.py
+++ src/training/train.py
@@ -150,7 +150,10 @@ def run_stratified_kfold(
             pretrained=True
         ).to(device)
         
-        criterion = BinaryFocalLoss(alpha=0.25, gamma=2.0)
+        #sotto: valori di alpha e gamma per primo set di addestramenti
+        # criterion = BinaryFocalLoss(alpha=0.25, gamma=2.0)
+        #sotto: nuovi valori di alpha e gamma per secondo set di addestramenti
+        criterion = BinaryFocalLoss(alpha=0.75, gamma=2.0)
         optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-2)
         
         # 5. Configurazione Warmup + Cosine Annealing Scheduler
